ce80935d1d63e8b37ca0c40433a0cc44a9886eae
SayakGithub01 <sayak.majumdermca@gmail.com>
2021-04-05 01:38:34 +0530

    added #

---
+++ appClientModule/class_test.java
@@ -0,0 +1,13 @@
+
+public class class_test {
+
+	public class_test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEllo world ####!!!");
+	}
+
+}
